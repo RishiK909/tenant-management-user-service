71a0c60032660f2d013574a4773f6394c8a69043
RishabhKumar <rishabhkumar.rk755@gmail.com>
2026-08-02 00:19:56 +0530

    Initial commit

---
+++ src/main/java/com/project/tenant_management_user_service/TenantManagementUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.tenant_management_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantManagementUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantManagementUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/tenant_management_user_service/TenantManagementUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tenant_management_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantManagementUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
