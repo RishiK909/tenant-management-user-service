f98616867d8ef2e8056f143223a96608b40fcfdf
RishabhKumar <rishabhkumar.rk755@gmail.com>
2026-08-02 02:23:46 +0530

    feat: Added update api, hasRole to create route and get-all route

--- src/main/java/com/project/tenant_management_user_service/service/TenantService.java
+++ src/main/java/com/project/tenant_management_user_service/service/TenantService.java
@@ -5,9 +5,15 @@ import com.project.tenant_management_user_service.dto.ApiResponse;
 import com.project.tenant_management_user_service.dto.TenantRegisterDTO;
 import com.project.tenant_management_user_service.entity.Tenant;
 
+import java.util.List;
+
 public interface TenantService {
 
     ApiResponse<Void> createTenantProfile(Long userId, TenantRegisterDTO request);
 
+    ApiResponse<Void> updateTenantProfile(Long userId, TenantRegisterDTO request);
+
     ApiResponse<Tenant> getMyProfile(Long userId);
+
+    ApiResponse<List<Tenant>> getAllTenants();
 }
